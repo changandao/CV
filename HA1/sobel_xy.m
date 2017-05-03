73b38c01af1f33fe57fffe073739c294fbce909c
sen.wang@tum.de <sen.wang@tum.de>
2017-05-03 22:11:15 +0200

    同志们，扛不住了，第三个要么是我电脑出了问题了，要么是我的matlab出了问题了
    同志们，扛不住了，第三个要么是我电脑出了问题了，要么是我的matlab出了问题了。调阈值怎么样也出不来正确结果

--- HA1/sobel_xy.m
+++ HA1/sobel_xy.m
@@ -2,8 +2,8 @@ function [Fx,Fy] = sobel_xy(Image)
 % In dieser Funktion soll das Sobel-Filter implementiert werden, welches
 % ein Graustufenbild einliest und den Bildgradienten in x- sowie in
 % y-Richtung zur?ckgibt.
-Sobel_x = [1, 0, -1; 2, 0, -2; 1, 0, -1]/8;
-Sobel_y = [1,2,1;0,0,0;-1,-2,-1]/8;
+Sobel_x = log(2)*[1, 0, -1; 2, 0, -2; 1, 0, -1]/8;
+Sobel_y = log(2)*[1,2,1;0,0,0;-1,-2,-1]/8;
 Fx = conv2(Image, Sobel_x, 'same');
 Fy = conv2(Image, Sobel_y, 'same');
 
